b30f4659b2a065b5011719f2bdbaa3a82d7da44a
Diego Mucheniski <mucheniski@gmail.com>
2022-07-21 14:02:34 -0300

    scripts de criacao de usuario

---
+++ files/ScriptsBanco.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.tb_user
+(user_id, "password", username)
+VALUES('37b3fabd-50a0-444b-a804-8601ef3e00c7', '$2a$10$VLsRfnxMKVzYEBfv8I36P.EKr2A5z029vMqTZXBJD0ikjFnInsQxW', 'noah');
+
+INSERT INTO public.tb_user
+(user_id, "password", username)
+VALUES('68a9707c-8f20-4b1e-8280-14d461325daa', '$2a$10$VLsRfnxMKVzYEBfv8I36P.EKr2A5z029vMqTZXBJD0ikjFnInsQxW', 'bruna');
