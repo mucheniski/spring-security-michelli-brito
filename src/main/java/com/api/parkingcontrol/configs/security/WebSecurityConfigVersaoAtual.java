392549ff9228039de39ac3521cbc327d51d0fa45
Diego Mucheniski <mucheniski@gmail.com>
2022-07-21 17:23:29 -0300

    configuracoes com controle de acessos no controller

--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfigVersaoAtual.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfigVersaoAtual.java
@@ -4,12 +4,14 @@ import com.api.parkingcontrol.enums.RoleNameEnum;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true) // Habilita a restrição direto no controller
 public class WebSecurityConfigVersaoAtual {
 
     public static final String PARKING_SPOT_BASE_URI = "/parking-spot";
